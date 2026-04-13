5da8baa7611d622bbc68ce39519263dcb9b37f6a
Andres <andresescalante1911@gmail.com>
2026-04-13 12:40:32 -0500

    Updates

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'notas-corazon-v1'
+const APP_SHELL_ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './pwa-192x192.svg',
+  './pwa-512x512.svg',
+  './vite.svg',
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_SHELL_ASSETS)
+    }),
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheKeys) => {
+      return Promise.all(
+        cacheKeys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key)),
+      )
+    }),
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request
+
+  if (request.method !== 'GET') {
+    return
+  }
+
+  if (new URL(request.url).origin !== self.location.origin) {
+    return
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse
+      }
+
+      return fetch(request).then((networkResponse) => {
+        const responseToCache = networkResponse.clone()
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(request, responseToCache)
+        })
+        return networkResponse
+      })
+    }),
+  )
+})
