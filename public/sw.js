3e8992611eae9ce9587c2cf59f724e9db9bb6326
Andres <andresescalante1911@gmail.com>
2026-04-13 12:55:37 -0500

    ultimos cambios

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'notas-corazon-v2'
+const CACHE_NAME = 'notas-corazon-v3'
 const APP_SHELL_ASSETS = [
   './',
   './index.html',
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,9 +4,9 @@ function AppHeader() {
       <p className="eyebrow">Para ti, con amor</p>
       <h1>Notas del Corazon</h1>
       <div className="header-flourish" aria-hidden="true">
-        <span>❤</span>
+        <span>♥</span>
         <span>✿</span>
-        <span>❤</span>
+        <span>♥</span>
       </div>
       <p className="subtitle">Una app pequeña para recordarte cuanto te amo.</p>
     </header>
